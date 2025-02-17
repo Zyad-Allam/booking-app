6e5dbe8598bce39228aedfc46b95617181fe68b8
Zyad Allam <zyad.wrk@gmail.com>
2025-02-17 14:05:01 +0300

    split main.go into functions and moved validate_user_input to helper

---
+++ helper.go
@@ -0,0 +1,11 @@
+package main
+
+import "strings"
+
+func validate_user_input(first_name string, last_name string, user_email string, user_tickets uint, remaining_tickets uint) (bool, bool, bool) {
+	is_valid_name := len(first_name) >= 2 && len(last_name) >= 2
+	is_valid_email := strings.Contains(user_email, "@")
+	is_valid_tickets := user_tickets > 0 && user_tickets <= remaining_tickets
+
+	return is_valid_name, is_valid_email, is_valid_tickets
+}
